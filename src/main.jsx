5a2fc9f42834490e04c512c2edaffbf29456818d
emeka-id <86531293+emeka-id@users.noreply.github.com>
2025-06-14 19:32:34 -0400

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+// main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Auth0Provider
+    domain={domain}
+    clientId={clientId}
+    authorizationParams={{
+      redirect_uri: redirectUri,
+    }}
+  >
+    <App />
+  </Auth0Provider>
+);
